3f25a83346bcb66e693bf15732438ca622e94b29
minjeongKoo <koominjeong99@gmail.com>
2021-05-01 20:53:23 +0900

    delete frontend directory add front directory and set proxy, test done (it's same set at frontend)

--- src/front/src/App.vue
+++ src/front/src/App.vue
@@ -1,19 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
+  <router-view/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import request from "request"
+import request from 'request'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
   mounted() {
     request('http://localhost:8080/api/hello',function (error, response, body) {
       window.console.log('error:', error);
@@ -31,6 +27,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
