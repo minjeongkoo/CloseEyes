e32f85a99ad644667353135275099a44e7ac92dd
minjeongKoo <koominjeong99@gmail.com>
2021-05-01 15:40:59 +0900

    Initial commit

---
+++ src/main/java/com/example/owproject/OwprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.owproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/owproject/OwprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.owproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
