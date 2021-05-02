851512dc141cbaaf8501e2a289459e4e36b64759
minjeongKoo <koominjeong99@gmail.com>
2021-05-02 14:13:59 +0900

    1. axios 모듈 다운로드 (i18n 기능때문에 넣었는데, 삭제할 수도 있음.) 2. header 영역 추가함 3. [common css] 폰트 import 해두었음.

--- src/front/src/App.vue
+++ src/front/src/App.vue
@@ -1,19 +1,35 @@
 <template>
+  <app-header/>
   <router-view/>
 </template>
 
+<script>
+import Header from "@/components/Header";
 
+export default {
+  components: {
+    'app-header': Header,
+  }
+}
+</script>
 
 <style>
+@import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css);
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Spoqa Han Sans Neo', 'sans-serif';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-
+body {
+  /*background: red;*/
+  padding-left: 320px;
+  padding-right: 320px;
+  background: #FAFAFA;
+}
 
 #nav {
   padding: 30px;
